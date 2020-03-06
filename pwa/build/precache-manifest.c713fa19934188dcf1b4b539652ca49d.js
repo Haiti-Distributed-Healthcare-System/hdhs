4251c833bc49369da4985f5940bf8c75cf2eec6d
Ben Mitchinson <mitchinson.dev@gmail.com>
2020-03-05 23:29:48 -0600

    just add the build

---
+++ pwa/build/precache-manifest.c713fa19934188dcf1b4b539652ca49d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c4714f0a9e2e92efd4b38dc2740e69d7",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9459db471a89bf697adc",
+    "url": "/static/css/main.6100c6c1.chunk.css"
+  },
+  {
+    "revision": "ae85eefaec4e755cf3aa",
+    "url": "/static/js/2.bdfd0dda.chunk.js"
+  },
+  {
+    "revision": "2ec056856ecbaa7b9e38b2ec2e56fa71",
+    "url": "/static/js/2.bdfd0dda.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9459db471a89bf697adc",
+    "url": "/static/js/main.15e38770.chunk.js"
+  },
+  {
+    "revision": "5f5e3d1442d3059e4bf2",
+    "url": "/static/js/runtime-main.1f8c0e43.js"
+  }
+]);
