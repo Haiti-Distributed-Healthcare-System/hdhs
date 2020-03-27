e8b83dbcb91303fbab12fabb7fed4fee97846877
Ben Mitchinson <mitchinson.dev@gmail.com>
2020-03-27 01:28:07 -0500

    useAPI hook with demonstration on the login component

---
+++ pwa/src/hooks/useApi.tsx
@@ -0,0 +1,26 @@
+import React from 'react'
+
+
+const useFetch = (route: string) => {
+  const [response, setResponse] = React.useState(null);
+  const [error, setError] = React.useState(null);
+  const [isLoading, setIsLoading] = React.useState(false);
+
+  React.useEffect(() => {
+    const fetchData = async () => {
+      setIsLoading(true);
+      try {
+        const res = await fetch("http://localhost:4000/" + route);
+        const json = await res.json();
+        setResponse(json);
+        setIsLoading(false)
+      } catch (error) {
+        setError(error);
+      }
+    };
+    fetchData();
+  }, []);
+  return { response, error, isLoading };
+};
+
+export default useFetch
