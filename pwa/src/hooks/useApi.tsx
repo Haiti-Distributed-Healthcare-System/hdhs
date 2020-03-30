200de534413730651801a25080e1fdce8ed32585
Ben Mitchinson <mitchinson.dev@gmail.com>
2020-03-30 16:47:26 -0500

    docker env variable to connect api in compose

--- pwa/src/hooks/useApi.tsx
+++ pwa/src/hooks/useApi.tsx
@@ -4,12 +4,21 @@ const useApi = (route: string) => {
   const [response, setResponse] = React.useState(null);
   const [error, setError] = React.useState(null);
   const [isLoading, setIsLoading] = React.useState(false);
+  
+  const getUrl = () => {
+    if (process.env.environment === "docker"){
+      return "http://api:4000/"
+    }
+    else {
+      return "http://localhost:4000/"
+    }
+  }
 
   React.useEffect(() => {
     const fetchData = async () => {
       setIsLoading(true);
       try {
-        const res = await fetch("http://localhost:4000/" + route);
+        const res = await fetch(getUrl() + route);
         const json = await res.json();
         setResponse(json);
         setIsLoading(false)
