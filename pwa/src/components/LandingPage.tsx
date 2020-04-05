7c631bd309348810d17e7713679710f12750bc4e
Meghan McLaughlin <meghan-l-mclaughlin@uiowa.edu>
2020-04-05 11:40:08 -0500

    started landing page element

---
+++ pwa/src/components/LandingPage.tsx
@@ -0,0 +1,20 @@
+import React, { ReactElement } from 'react'
+import {WhiteSpace, Button, Icon} from 'antd-mobile'
+
+import '../scss/Login.scss'
+
+export default function Login(): ReactElement {
+  const patientLookup = () => {
+    console.log("Clicked Patient Lookup")
+  }
+
+  const newPatient = () => {
+    console.log("Clicked New Patient")
+  }
+  return (
+    <>
+      <Button onClick={patientLookup} icon="search">Patient Lookup</Button><WhiteSpace />
+      <Button onClick={newPatient} icon="comment">New Patient</Button><WhiteSpace />
+    </>
+  )
+}
