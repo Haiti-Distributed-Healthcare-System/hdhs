c3c3de452d160efa8a637be7007920ba9927a7c5
abpwrs <powersa15@gmail.com>
2020-04-08 15:33:33 -0500

    ENH: consolidating bash scripts

---
+++ db/execute-sql-in-container.bash
@@ -0,0 +1,22 @@
+#!/usr/bin/env bash
+# Author: "hertz"
+# Date: 20200310
+# ####################
+# DESC: 
+#   A script to execute a SQL script inside of postgres docker container
+# ####################
+
+
+# args:
+# 1 -- name of container running postgres to execute the script inside of 
+# 2 -- path to .SQL script to be execute inside of the postgres container
+
+#if [[ $# -ne 1 ]]; then
+#    echo "Usage: $0 <param-1> <param-2> ..."
+#    exit
+#fi
+
+
+# script:
+
+
