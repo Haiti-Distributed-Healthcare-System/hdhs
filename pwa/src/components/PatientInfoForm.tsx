8eb3759c3d90e0deae1a0b6e79f0988b8db27dd1
Meghan McLaughlin <meghan-l-mclaughlin@uiowa.edu>
2020-04-08 12:33:53 -0500

    not compiling; error with typing

--- pwa/src/components/PatientInfoForm.tsx
+++ pwa/src/components/PatientInfoForm.tsx
@@ -1,15 +1,15 @@
-import React, { ReactElement } from 'react'
-import {DatePicker, InputItem, List, TextareaItem, Button} from 'antd-mobile'
+import React, { ReactElement, useState, useEffect } from 'react'
+import {DatePicker, InputItem, List, TextareaItem, Button, Radio} from 'antd-mobile'
 import enUs from 'antd-mobile/lib/date-picker/locale/en_US';
 import '../scss/Login.scss'
-import { render } from '@testing-library/react';
+// import { render } from '@testing-library/react';
+const RadioItem = Radio.RadioItem;
 
-// const nowTimeStamp = Date.now();
-// const now = new Date(nowTimeStamp);
 
 export default function Form(): ReactElement {
-    // // useState returns a value, and a function to set that value
-    // const [name, setName] = useState()
+    // useState returns a value, and a function to set that value
+    const [name, setName] = useState()
+    const [planning, setPlanning] = useState(0)
 
     const onSubmit = () => {
       // This shows how to access the values of the form
@@ -26,134 +26,145 @@ export default function Form(): ReactElement {
       console.log("Complaints: ", complaintField1.value + " " + complaintField2.value + " " + complaintField3.value)
     }
 
+    const wantsPlanning = [
+        { value: 0, label: 'Yes' },
+        { value: 1, label: 'No' },
+    ];
+
+    const onChange = (value :any) => { //todo change type here
+        console.log('checkbox');
+    };
+
     return (
-      <>
-      <TextareaItem
-      title="First Name"
-      placeholder="First Name"
-      data-seed="firstName"
-      autoHeight
-      id="firstname"
-      />
+      <div id='patient-info-wrapper'>
+        <TextareaItem
+        title="First Name"
+        placeholder="First Name"
+        data-seed="firstName"
+        autoHeight
+        id="firstname"
+        />
 
-      <TextareaItem
-      title="Last Name"
-      placeholder="Last Name"
-      data-seed="lastName"
-      id="lastname"
-      // whats the difference between id and data seed?
-      autoHeight
-      />
+        <TextareaItem
+        title="Last Name"
+        placeholder="Last Name"
+        data-seed="lastName"
+        id="lastname"
+        // whats the difference between id and data seed?
+        autoHeight
+        />
 
-      <TextareaItem
-      title="Nickname"
-      placeholder="optional"
-      data-seed="nickname"
-      id="nickname"
-      autoHeight
-      />
+        <TextareaItem
+        title="Nickname"
+        placeholder="optional"
+        data-seed="nickname"
+        id="nickname"
+        autoHeight
+        />
 
-      <DatePicker
-      mode="date"
-      locale={enUs}
-      title="Birth Date"
-      extra="Birth Date"
-      data-seed="birthDate"
-      >
-      <List.Item arrow="horizontal">Birth Date</List.Item>
-      {/* TODO: figure out how to access this value */}
-      </DatePicker>
-      <InputItem
-          type="money"
-          moneyKeyboardAlign="left"
-          clear
-          id = 'age'
-      >Age </InputItem>
+        <DatePicker
+        mode="date"
+        locale={enUs}
+        title="Birth Date"
+        extra="Birth Date"
+        data-seed="birthDate"
+        >
+        <List.Item arrow="horizontal">Birth Date</List.Item>
+        {/* TODO: figure out how to access this value */}
+        </DatePicker>
+        <InputItem
+            type="money"
+            moneyKeyboardAlign="left"
+            clear
+            id = 'age'
+        >Age </InputItem>
 
-      <InputItem
-          type="money"
-          moneyKeyboardAlign="left"
-          clear
-          id = 'phone'
-      >Phone</InputItem>
+        <InputItem
+            type="money"
+            moneyKeyboardAlign="left"
+            clear
+            id = 'phone'
+        >Phone</InputItem>
 
-      {/* TODO: only display this section for patients marked as Female */}
-      <List>
-          <TextareaItem
-              title="G"
-              id = 'gravida'
-          />
-          <TextareaItem
-              title="P"
-              id = 'para'
-          />
-          <TextareaItem
-              title="A"
-              id = 'abortus'
-          />
-          <TextareaItem
-              title="LMP"
-              id = 'lmp'
-          />
-          <TextareaItem
-            // TODO: this should be a radio buttin
-              title="Wants Planning"
-              id = 'wants_planning'
-          />
-      </List>
+        {/* TODO: only display this section for patients marked as Female */}
+        <div id='female-only'>
+            <TextareaItem
+                title="G"
+                id = 'gravida'
+            />
+            <TextareaItem
+                title="P"
+                id = 'para'
+            />
+            <TextareaItem
+                title="A"
+                id = 'abortus'
+            />
+            <TextareaItem
+                title="LMP"
+                id = 'lmp'
+            />
+            <List renderHeader={() => 'Wants Planning?'}>
+                {wantsPlanning.map(i => (
+                    <RadioItem key={i.value} checked={this.state.planning  === i.value} onChange={() => onChange(i.value)}>
+                        {i.label}
+                    </RadioItem>
+                ))}
+            </List>
+        </div>
 
-      {/* these don't technically need to be in their own <List/> */}
+        {/* these don't technically need to be in their own <List/> */}
 
-      <List>
-          <InputItem
-          type="money"
-          moneyKeyboardAlign="left"
-          clear
-          id = 'weight'
-          >Weight (kg)</InputItem>
+        <List>
+            <InputItem
+            type="money"
+            moneyKeyboardAlign="left"
+            clear
+            id = 'weight'
+            >Weight (kg)</InputItem>
 
-          <InputItem
-          type="money"
-          moneyKeyboardAlign="left"
-          clear
-          id = 'height'
-          >Height (cm)</InputItem>
+            <InputItem
+            type="money"
+            moneyKeyboardAlign="left"
+            clear
+            id = 'height'
+            >Height (cm)</InputItem>
 
-          <InputItem
-          type="money"
-          moneyKeyboardAlign="left"
-          clear
-          id = 'temp'
-          >Temp (F)</InputItem>
+            <InputItem
+            type="money"
+            moneyKeyboardAlign="left"
+            clear
+            id = 'temp'
+            >Temp (F)</InputItem>
 
-          <InputItem
-          type="money"
-          moneyKeyboardAlign="left"
-          clear
-          id = 'pulse'
-          >Pulse</InputItem>
-          {/* TODO: figure out how to access these values from the money keyboard */}
-      </List>
-      <List renderHeader={() => 'Chief Complaint'}>
-          <TextareaItem
-              placeholder="1."
-              // autoHeight
-              rows={2}
-              id = 'complaint1'
-          />
-          <TextareaItem
-              placeholder="2."
-              rows={2}
-              id = 'complaint2'
-          />
-          <TextareaItem
-              placeholder="3."
-              rows={2}
-              id = 'complaint3'
-          />
-      </List>
-        <Button onClick={onSubmit} >Submit</Button>
+            <InputItem
+            type="money"
+            moneyKeyboardAlign="left"
+            clear
+            id = 'pulse'
+            >Pulse</InputItem>
+            {/* TODO: figure out how to access these values from the money keyboard */}
+        </List>
+        <List renderHeader={() => 'Chief Complaint'}>
+            <TextareaItem
+                placeholder="1."
+                // autoHeight
+                rows={2}
+                id = 'complaint1'
+            />
+            <TextareaItem
+                placeholder="2."
+                rows={2}
+                id = 'complaint2'
+            />
+            <TextareaItem
+                placeholder="3."
+                rows={2}
+                id = 'complaint3'
+            />
+        </List>
+            <Button onClick={onSubmit} >Submit</Button>
 
-    </>
+    </div>
     )
 }
