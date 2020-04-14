b81c161a0806e572c79774cc5ad19b1d44739e7e
Ben Mitchinson <mitchinson.dev@gmail.com>
2020-04-13 21:04:15 -0500

    demo navbar

--- pwa/src/Wrapper.tsx
+++ pwa/src/Wrapper.tsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { NavBar, Icon } from "antd-mobile";
 
 import "./scss/Wrapper.scss";
 
@@ -9,7 +10,20 @@ type props = {
 const Wrapper = (props: props) => {
   return (
     <div id="primary-wrapper">
-      <div id="wrapper-contents">{props.children}</div>
+      <div id="wrapper-contents">
+        <NavBar
+          mode="light"
+          icon={<Icon type="left" />}
+          onLeftClick={() => console.log("onLeftClick")}
+          rightContent={[
+            <Icon key="0" type="search" style={{ marginRight: "16px" }} />,
+            <Icon key="1" type="ellipsis" />,
+          ]}
+        >
+          NavBar
+        </NavBar>
+        {props.children}
+      </div>
     </div>
   );
 };
