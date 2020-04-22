a83c589c4ad33082542d94f051c00f847c0bda3f
Meghan McLaughlin <meghan-l-mclaughlin@uiowa.edu>
2020-04-22 11:13:36 -0500

    populated form with diagnosis name

--- pwa/src/components/DiagnosisForm.tsx
+++ pwa/src/components/DiagnosisForm.tsx
@@ -1,6 +1,10 @@
 import React, { ReactElement, useState } from "react";
-import {} from "antd-mobile";
+import { List, Checkbox, Flex } from 'antd-mobile';
 import "../scss/DiagnosisForm.scss";
+import * as data from './DiagnosisFields.json';
+
+const CheckboxItem = Checkbox.CheckboxItem;
+const AgreeItem = Checkbox.AgreeItem;
 
 /*
 
@@ -24,12 +28,36 @@ Diagnosis Fields are stored in DiagnosisFields.json in the format:
     ]
 }
 */
+// var diagnosisFields = require('./DiagnosisFields.json');
+// import * as data from './DiagnosisFields.json';
+
+var diagnosisFields = data.diagnoses;
+
+var fieldComponents = [];
+
+function createFields() {
+    diagnosisFields.forEach((diagnosisField) => {
+        // Create the corresponding element and push it to the array of fields
+    });
+}
 
 export default function Form(): ReactElement {
 
     return (
-        <>
+        <div>
 
-        </>
+            <List renderHeader={() => 'CheckboxItem demo'}>
+                {diagnosisFields.map((i) => (
+                    // TOOD: how do I add conditionals here?
+                    <CheckboxItem
+                      id={`${i.name.toLowerCase()}-button`}
+                      key={i.name}
+                      name="sex"
+                    >
+                      {i.name}
+                    </CheckboxItem>
+                  ))}
+            </List>
+        </div>
     )
 }
