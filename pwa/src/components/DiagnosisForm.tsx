00669a3e4e41b55d9058887b4d1f399dfa693fc7
Meghan McLaughlin <meghan-l-mclaughlin@uiowa.edu>
2020-04-21 21:52:13 -0500

    created json format for form fields

--- pwa/src/components/DiagnosisForm.tsx
+++ pwa/src/components/DiagnosisForm.tsx
@@ -2,9 +2,34 @@ import React, { ReactElement, useState } from "react";
 import {} from "antd-mobile";
 import "../scss/DiagnosisForm.scss";
 
+/*
+
+Diagnosis Fields are stored in DiagnosisFields.json in the format:
+
+{
+    "diagnoses" : [
+        {
+            "name":"",
+            "group-title":"",
+            "group":[],
+            "radio-buttons": {
+                "radio-buttons-title":"",
+                "radio-buttons":[
+
+                ]
+
+            },
+            "text-input-title":""
+        }
+    ]
+}
+*/
+
 export default function Form(): ReactElement {
+
     return (
         <>
+
         </>
     )
 }
