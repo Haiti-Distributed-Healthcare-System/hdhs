40f2611436a14de200a3c3cc53eeb858b0acf34d
Meghan McLaughlin <meghan-l-mclaughlin@uiowa.edu>
2020-04-29 12:48:13 -0500

    start of testing

--- pwa/src/components/TestResultsForm.test.tsx
+++ pwa/src/components/TestResultsForm.test.tsx
@@ -1,3 +1,45 @@
 import React from 'react'
-import { render, fireEvent } from '@testing-library/react'
+import { render, fireEvent, getByTestId } from '@testing-library/react'
 import TestResultsForm from './TestResultsForm'
+
+const testText = [
+    'HCG \\(Pregnancy\\)',
+    'Blood Sugar',
+    'UA \\(Urine Analysis\\)',
+]
+
+const testID = ['pregnancy-test', 'blood-sugar-test', 'ua-test']
+
+test('renders the form', () => {
+    const dom = render(<TestResultsForm />)
+    expect(dom.getByTestId('test-results-form-wrapper')).toBeTruthy()
+})
+
+test('renders each test option as a checkbox and is able to be checked / unchecked', () => {
+    const { getByText, getByTestId } = render(<TestResultsForm />)
+
+    testText.forEach((testName) => {
+        const re = new RegExp(testName, 'gi')
+        const field = getByText(re)
+        expect(field).toBeInTheDocument()
+    })
+
+    testID.forEach((testID) => {
+        const checkboxItem = getByTestId(testID)
+        expect(checkboxItem).toHaveClass('am-checkbox-item')
+
+        const checkBox = checkboxItem.getElementsByTagName('span')[0]
+        expect(checkBox).not.toHaveClass('am-checkbox-checked')
+        const checkBoxInput = checkboxItem.getElementsByClassName(
+            'am-checkbox-input',
+        )[0]
+        fireEvent.click(checkBoxInput)
+        expect(checkBox).toHaveClass('am-checkbox-checked')
+    })
+})
+
+test('pregnancy-results div is only displayed if pregnancy test is selected', () => {})
+
+test('blood-sugar-results div is only displayed if blood sugar test is selected', () => {})
+
+test('ua-results div is only displayed if UA test is selected', () => {})
--- pwa/src/components/TestResultsForm.tsx
+++ pwa/src/components/TestResultsForm.tsx
@@ -10,7 +10,7 @@ export default function TestResultsForm(): ReactElement {
     const [pregnancyTest, setPregnancyTest] = useState(0) // Default: 0 (unchecked)
     const [pregnancyTestPositive, setPregnancyTestPositive] = useState(
         null as number,
-    ) // Default: null
+    ) // Default: null (none selected)
     const [bloodSugarTest, setBloodSugarTest] = useState(0) // Default: 0 (unchecked)
     const [ua, setUA] = useState(0) // Default: 0 (unchecked)
 
@@ -21,82 +21,91 @@ export default function TestResultsForm(): ReactElement {
     ]
 
     return (
-        <div id="test-results-form-wrapper">
-            <CheckboxItem
-                id="pregnancy_test"
-                data-testid="pregnancy_test"
-                onChange={() => setPregnancyTest(pregnancyTest ? 0 : 1)}
-            >
-                HCG (Pregnancy) Test Performed
-            </CheckboxItem>
-            {pregnancyTest ? (
-                <div id="pregnancy_results">
-                    <List renderHeader={() => 'HCG (Pregnancy) Results'}>
-                        {radioPosNeg.map((i) => (
-                            <RadioItem
-                                key={i.value}
-                                data-testid={`pregnancy_results-${i.label.toLowerCase}`}
-                                name="pregnancy_results_radio"
-                                checked={pregnancyTestPositive === i.value}
-                                onClick={() =>
-                                    setPregnancyTestPositive(i.value)
-                                }
+        <div
+            id="test-results-form-wrapper"
+            data-testid="test-results-form-wrapper"
+        >
+            <List>
+                {' '}
+                <CheckboxItem
+                    id="pregnancy-test"
+                    data-testid="pregnancy-test"
+                    onChange={() => setPregnancyTest(pregnancyTest ? 0 : 1)}
+                    title="HCG (Pregnancy) Test Performed"
+                >
+                    HCG (Pregnancy) Test Performed
+                </CheckboxItem>
+                {pregnancyTest ? (
+                    <div id="pregnancy-results">
+                        <List renderHeader={() => 'HCG (Pregnancy) Results'}>
+                            {radioPosNeg.map((i) => (
+                                <RadioItem
+                                    key={i.value}
+                                    data-testid={`pregnancy-results-${i.label.toLowerCase}`}
+                                    name="pregnancy-results-radio"
+                                    checked={pregnancyTestPositive === i.value}
+                                    onClick={() =>
+                                        setPregnancyTestPositive(i.value)
+                                    }
+                                >
+                                    {i.label}
+                                </RadioItem>
+                            ))}
+                            <WhiteSpace className="list-whitespace" size="lg" />
+                        </List>
+                    </div>
+                ) : null}
+                <CheckboxItem
+                    checked={bloodSugarTest}
+                    id="blood-sugar-test"
+                    data-testid="blood-sugar-test"
+                    onChange={() => setBloodSugarTest(bloodSugarTest ? 0 : 1)}
+                >
+                    Blood Sugar Test Performed
+                </CheckboxItem>
+                {bloodSugarTest ? (
+                    <div id="blood-sugar-results">
+                        <List renderHeader={() => 'Blood Sugar Results'}>
+                            <InputItem
+                                type="number"
+                                id="blood-sugar-results-val"
+                                placeholder="mg/dL" // TOOD: unsure if the unit is mg/dL or mmol/L, could leave this out
+                                maxLength={3}
+                            ></InputItem>
+                            <WhiteSpace className="list-whitespace" size="lg" />
+                        </List>
+                    </div>
+                ) : null}
+                <CheckboxItem
+                    id="ua-test"
+                    data-testid="ua-test"
+                    onChange={() => setUA(ua ? 0 : 1)}
+                >
+                    UA (Urine Analysis) Test Performed
+                </CheckboxItem>
+                {ua ? (
+                    <div id="ua-results">
+                        <List renderHeader={() => 'UA Results'}>
+                            {/* TODO: I am unsure about the range/type of these fields */}
+                            <InputItem type="number" id="glucose-results-val">
+                                Glucose
+                            </InputItem>
+                            <InputItem type="number" id="nitrites-results-val">
+                                Nitrites
+                            </InputItem>
+                            <InputItem type="number" id="protein-results-val">
+                                Protein
+                            </InputItem>
+                            <InputItem
+                                type="number"
+                                id="leukocytes-results-val"
                             >
-                                {i.label}
-                            </RadioItem>
-                        ))}
-                        <WhiteSpace className="list-whitespace" size="lg" />
-                    </List>
-                </div>
-            ) : null}
-
-            <CheckboxItem
-                checked={bloodSugarTest}
-                id="blood_sugar_test"
-                data-testid="blood_sugar_test"
-                onChange={() => setBloodSugarTest(bloodSugarTest ? 0 : 1)}
-            >
-                Blood Sugar Test Performed
-            </CheckboxItem>
-            {bloodSugarTest ? (
-                <div id="blood_sugar_results">
-                    <List renderHeader={() => 'Blood Sugar Results'}>
-                        <InputItem
-                            type="number"
-                            id="blood_sugar_results_val"
-                            placeholder="mg/dL" // TOOD: unsure if the unit is mg/dL or mmol/L, could leave this out
-                            maxLength={3}
-                        ></InputItem>
-                    </List>
-                </div>
-            ) : null}
-
-            <CheckboxItem
-                id="ua_test"
-                data-testid="ua_test"
-                onChange={() => setUA(ua ? 0 : 1)}
-            >
-                UA (Urine Analysis) Test Performed
-            </CheckboxItem>
-            {ua ? (
-                <div id="ua_results">
-                    <List renderHeader={() => 'UA Results'}>
-                        {/* TODO: I am unsure about the range/type of these fields */}
-                        <InputItem type="number" id="glucose_results_val">
-                            Glucose
-                        </InputItem>
-                        <InputItem type="number" id="nitrites_results_val">
-                            Nitrites
-                        </InputItem>
-                        <InputItem type="number" id="protein_results_val">
-                            Protein
-                        </InputItem>
-                        <InputItem type="number" id="leukocytes_results_val">
-                            Leukocytes
-                        </InputItem>
-                    </List>
-                </div>
-            ) : null}
+                                Leukocytes
+                            </InputItem>
+                        </List>
+                    </div>
+                ) : null}
+            </List>
         </div>
     )
 }
