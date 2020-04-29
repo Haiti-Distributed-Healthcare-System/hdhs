817dbc03470209b9d7101b968e66e0ac72d7186f
Ben Mitchinson <mitchinson.dev@gmail.com>
2020-04-29 12:57:45 -0500

    patientInfoForm interface reflects FMP

--- pwa/src/state/models/patientInfoForm.model.ts
+++ pwa/src/state/models/patientInfoForm.model.ts
@@ -2,33 +2,35 @@ import { Action, action } from 'easy-peasy'
 import { SEX, YN } from '../enums'
 
 interface PatientInfoFormFields {
-    firstName?: string
-    lastName?: string
+    first_name?: string
+    last_name?: string
     nickname?: string
-    sex?: SEX
-    birthDate?: Date
+    gender?: SEX
+    birthdate?: Date
     age?: number
     phone?: string
     town?: string
-    firstVisit?: YN
-    pregnant?: YN
-    G?: number
-    P?: number
-    A?: number
-    LMP?: Date
-    wantsPlanning?: YN
-    weight?: number
-    height?: number
-    bloodPressure?: string
-    temperature?: number
-    pulse?: number
+    first_visit?: YN
+    ob_pregnant?: YN
+    ob_gravida?: number
+    ob_para?: number
+    ob_abortus?: number
+    ob_last_menstrual_period?: Date
+    ob_wants_planning?: YN
+    weight_in_lbs?: number
+    height_in_inches?: number
+    bp_systolic_blood_pressure?: number
+    bp_diastolic_blood_pressure?: number
+    temp_in_fahrenheit?: number
+    pulse_rate?: number
+    // TODO: Can't find zScore proper name?
     zScore?: number
-    albendazole?: YN
-    allergies?: string
-    medicines?: string
-    chiefComplaint?: string[]
-    history?: string
-    exam?: string
+    albendazole_given?: YN
+    known_med_allergies?: string
+    meds_currently_taken?: string
+    chief_complaint?: string
+    history_of_present_illness?: string
+    exam_comments?: string
 }
 
 export interface PatientInfoFormModel {
