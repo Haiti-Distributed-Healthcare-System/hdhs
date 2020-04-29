a259feba406e8f6ea07d3c172ab572b93e342fd4
Meghan McLaughlin <meghan-l-mclaughlin@uiowa.edu>
2020-04-29 09:43:01 -0500

    init

---
+++ pwa/src/components/TestResultsForm.test.tsx
@@ -0,0 +1,3 @@
+import React from 'react'
+import { render, fireEvent } from '@testing-library/react'
+import TestResultsForm from './TestResultsForm'
---
+++ pwa/src/components/TestResultsForm.tsx
@@ -0,0 +1,7 @@
+import React, { ReactElement } from 'react'
+import {} from 'antd-mobile'
+import '../scss/DiagnosisForm.scss'
+
+export default function DiagnosisForm(): ReactElement {
+    return <></>
+}
