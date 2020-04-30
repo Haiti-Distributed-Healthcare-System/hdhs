e41a5220e910d72b59e935f66f735299117221c1
Zain Khan <zain.kha.al@gmail.com>
2020-04-30 04:10:52 -0500

    Treatment form complete, tests remaining

--- pwa/src/components/TreatmentForm.tsx
+++ pwa/src/components/TreatmentForm.tsx
@@ -31,9 +31,8 @@ export default function TreatmentForm(): ReactElement {
         <div id="treatment-form-wrapper" data-testid="treatment-form-wrapper">
             {treatmentFields.map((field) => {
                 const formElements: ReactElement[] = []
-
-                if (
-                    field['text-input-title'] != null && field['text-input-id'] != null) {
+                    
+                if (field['text-input-title'] != null && field['text-input-id'] != null) {
                     formElements.push(
                         <List renderHeader={() => field['text-input-title']}>
                             <InputItem
@@ -64,21 +63,32 @@ export default function TreatmentForm(): ReactElement {
                                 </CheckboxItem>,
                             )
                         }
+
+                        if (ele['text-input-title'] != null && ele['text-input-id'] != null) {
+                            groupElements.push(
+                                    <TextareaItem
+                                        id={ele['text-input-id']}
+                                        placeholder={ele['text-input-title']}
+                                        data-testid={ele['text-input-id']}
+                                        key={ele['text-input-id']}
+                                        maxLength={null}
+                                    />
+                            )
+                        }
                     })
 
                     // Render the entire list with the internal elements
                     if (field['group-title'] != null && field.id !== null) {
                         formElements.push(
-                            <List
-                                renderHeader={() => field['group-title']}
-                                data-testid={field.id}
-                            >
-                                {groupElements}
-                                <WhiteSpace
-                                    className="list-whitespace"
-                                    size="lg"
-                                />
-                            </List>,
+                            <div>
+                                <Accordion data-testid={field.id}>
+                                <Accordion.Panel 
+                                    header={field['group-title']}
+                                >
+                                    {groupElements}
+                                </Accordion.Panel>
+                                </Accordion>
+                            </div>
                         )
                     }
                 }
@@ -87,108 +97,4 @@ export default function TreatmentForm(): ReactElement {
             })}
         </div>
     )
-         {/*}   
-            <Accordion defaultActiveKey="0" className="my-accordion">
-            <Accordion.Panel header="Multivitamins">
-                <CheckboxItem>Multivitamins - Childrens</CheckboxItem>
-                <CheckboxItem>Multivitamins - Adult</CheckboxItem>
-                <CheckboxItem>Multivitamins - Prenatal</CheckboxItem>
-            </Accordion.Panel>
-            <Accordion.Panel header="Pain">
-                <CheckboxItem>Acetaminophen - 500 tab</CheckboxItem>
-                <CheckboxItem>Acetaminophen - 80 chew</CheckboxItem>
-                <CheckboxItem>Acetaminophen - 160 mg/5 cc</CheckboxItem>
-                <CheckboxItem>Ibuprofen - 200 mg</CheckboxItem>
-                <CheckboxItem>Ibuprofen - 100 mg/5 cc</CheckboxItem>
-            </Accordion.Panel>            
-            <Accordion.Panel header="GI">
-                <CheckboxItem>Omeprazole</CheckboxItem>
-                <CheckboxItem>Pepto Bismol</CheckboxItem>
-                <CheckboxItem>Ranitdine</CheckboxItem>
-                <CheckboxItem>Zinc</CheckboxItem>
-                <CheckboxItem>Tums</CheckboxItem>
-                <CheckboxItem>ORS</CheckboxItem>
-            </Accordion.Panel>
-            <Accordion.Panel header="Cardiac">
-                <CheckboxItem>Amlodipine</CheckboxItem>
-                <CheckboxItem>Aspirin</CheckboxItem>
-                <CheckboxItem>Atenolol</CheckboxItem>
-                <CheckboxItem>Lisinopril</CheckboxItem>
-                <CheckboxItem>HCTZ</CheckboxItem>
-                <CheckboxItem>Lasix tab</CheckboxItem>
-            </Accordion.Panel>
-            <Accordion.Panel header="Anti-infectives">
-                <CheckboxItem>Albendazole</CheckboxItem>
-                <CheckboxItem>Amoxicillin - 500 tab</CheckboxItem>
-                <CheckboxItem>Amoxicillin - 250 chew</CheckboxItem>
-                <CheckboxItem>Amoxicillin - 250 mg/cc</CheckboxItem>
-                <CheckboxItem>Augmentin</CheckboxItem>
-                <CheckboxItem>Azithromycin - 250 tab</CheckboxItem>
-                <CheckboxItem>Azithromycin - 200 mg/5 cc</CheckboxItem>
-                <CheckboxItem>Bactrim - DS tab</CheckboxItem>
-                <CheckboxItem>Bactrim - 40 mg/5 cc</CheckboxItem>
-                <CheckboxItem>Cipro</CheckboxItem>
-                <CheckboxItem>Chloroquine</CheckboxItem>                
-                <CheckboxItem>Doxycycline</CheckboxItem>
-                <CheckboxItem>Flagyl</CheckboxItem>
-                <CheckboxItem>Fluconazole</CheckboxItem>
-                <CheckboxItem>Griseofulvin - 500 mg</CheckboxItem>
-                <CheckboxItem>Griseofulvin - 125 mg</CheckboxItem>
-                <CheckboxItem>Ivermectin</CheckboxItem>
-                <CheckboxItem>Keflex 500 mg tab</CheckboxItem>
-                <CheckboxItem>Nitrofurantoin</CheckboxItem>                
-                <CheckboxItem>Rocephin Inj. - 50 mg/kg</CheckboxItem>
-                <CheckboxItem>Rocephin Inj. - 250 mg</CheckboxItem>
-                <CheckboxItem>Rocephin Inj. - 1 gm</CheckboxItem>
-            </Accordion.Panel>
-            <Accordion.Panel header="Topical">
-                <CheckboxItem>Clotrimazole</CheckboxItem>
-                <CheckboxItem>Triple Antibiotic</CheckboxItem>
-                <CheckboxItem>Hydrocortisone</CheckboxItem>
-                <CheckboxItem>Permethrine</CheckboxItem>
-                <CheckboxItem>Silvadine</CheckboxItem>
-            </Accordion.Panel>
-            <Accordion.Panel header="IM">
-                <CheckboxItem>Dexamethasone</CheckboxItem>
-                <CheckboxItem>Lasix Injection</CheckboxItem>
-            </Accordion.Panel>
-            <Accordion.Panel header="Misc.">
-                <CheckboxItem>Albuterol</CheckboxItem>
-                <CheckboxItem>Benadryl - 25 mg</CheckboxItem>
-                <CheckboxItem>Benadryl - 12.5 mg/5 cc</CheckboxItem>
-                <CheckboxItem>Depo-Provera</CheckboxItem>
-                <CheckboxItem>Ferrous sulfate</CheckboxItem>
-                <CheckboxItem>Paroxetine</CheckboxItem>
-                <CheckboxItem>Labetolol</CheckboxItem>
-                <CheckboxItem>Methyldopa</CheckboxItem>
-                <CheckboxItem>Metformin</CheckboxItem>
-                <CheckboxItem>OCP</CheckboxItem>
-                <CheckboxItem>IUD</CheckboxItem>
-                <CheckboxItem>Carbamazepine</CheckboxItem>
-                <CheckboxItem>Keppra</CheckboxItem>
-                <CheckboxItem>Prednisone</CheckboxItem>
-                <CheckboxItem>Terazosin</CheckboxItem>
-                <TextareaItem 
-                    placeholder="Other misc.:" 
-                    data-seed="other-misc" 
-                    autoHeight 
-                    id="other-misc"/>
-            </Accordion.Panel>
-            <Accordion.Panel header="Referrals">
-                <CheckboxItem>Saw dentist</CheckboxItem>
-                <CheckboxItem>Saw eye doctor</CheckboxItem>
-                <CheckboxItem>CHW</CheckboxItem>
-                <CheckboxItem>Given Glasses</CheckboxItem>
-                <CheckboxItem>Outside</CheckboxItem>
-            </Accordion.Panel>
-            <Accordion.Panel header="Comments">
-                <TextareaItem 
-                    placeholder="Treatment Comments" 
-                    data-seed="comments" 
-                    autoHeight 
-                    id="comments"/>
-            </Accordion.Panel>
-            </Accordion>
-        
-    */}
 }
